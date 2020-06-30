d05a1552afe7c2254b41d35988966e1f446e8b87
copiousamounts <nick.hernstedt@gmail.com>
2020-06-30 10:50:26 -0700

    added form search bar

--- grubster/src/App.js
+++ grubster/src/App.js
@@ -2,8 +2,22 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const APP_ID = "af1bb2ea";
+  const APP_KEY = "35d9ba4f9d829c6e667107b220c80c39";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
